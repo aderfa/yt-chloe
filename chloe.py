5b67e9f9d3e881ae1d5d3cc65178e74f28d658aa
aderfa <77231874+aderfa@users.noreply.github.com>
2024-08-09 02:04:54 +0700

    Update chloe.py

--- chloe.py
+++ chloe.py
@@ -1,31 +1,31 @@
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from google.auth.transport.requests import Request  # Import this module
+from google.auth.transport.requests import Request 
 from datetime import datetime
 import pytz
 import os
 import pickle
 
-# Ganti dengan path ke file kredensial OAuth 2.0 Anda
+# File OAuth 2.0
 CLIENT_SECRETS_FILE = 'test.json'
 TOKEN_PICKLE_FILE = 'token.pickle'
-# Ganti 'VIDEO_ID' dengan ID video yang ingin Anda ubah judulnya
+
 VIDEO_ID = 'YGzH8dgRjl8'
 
-# Tanggal mulai
+# Tanggal mulai stream chloe
 START_DATE = datetime(2023, 5, 14, tzinfo=pytz.timezone('Asia/Jakarta'))
 
-# OAuth 2.0 Scopes
+
 SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
 
 def get_authenticated_service():
     credentials = None
-    # Cek apakah token sudah ada
+    
     if os.path.exists(TOKEN_PICKLE_FILE):
         with open(TOKEN_PICKLE_FILE, 'rb') as token:
             credentials = pickle.load(token)
-    # Jika tidak ada token atau token sudah expired, lakukan otorisasi
+   
     if not credentials or not credentials.valid:
         if credentials and credentials.expired and credentials.refresh_token:
             credentials.refresh(Request())
@@ -33,7 +33,7 @@ def get_authenticated_service():
             flow = InstalledAppFlow.from_client_secrets_file(
                 CLIENT_SECRETS_FILE, SCOPES)
             credentials = flow.run_local_server(port=0)
-        # Simpan token ke file
+        
         with open(TOKEN_PICKLE_FILE, 'wb') as token:
             pickle.dump(credentials, token)
     return build('youtube', 'v3', credentials=credentials)
@@ -45,17 +45,17 @@ def get_days_since_start():
 
 def change_video_title(youtube, video_id, new_title):
     try:
-        # Ambil detail video
+        
         video_response = youtube.videos().list(
             part='snippet',
             id=video_id
         ).execute()
 
-        # Ubah judul video
+        
         video_snippet = video_response['items'][0]['snippet']
         video_snippet['title'] = new_title
 
-        # Update video
+        
         update_response = youtube.videos().update(
             part='snippet',
             body={
@@ -73,9 +73,9 @@ def change_video_title(youtube, video_id, new_title):
 if __name__ == '__main__':
     youtube = get_authenticated_service()
     
-    # Hitung jumlah hari
+    
     days_since_start = get_days_since_start()
     new_title = f"Menunggu Chloe Stream Day {days_since_start}"
     
-    # Ubah judul video
+    
     change_video_title(youtube, VIDEO_ID, new_title)
